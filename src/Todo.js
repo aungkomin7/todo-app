8e093f256ddd9115d7afe31ec95b9abcf8b231ce
aungkomin7 <winfloweraungkomin1997@gmail.com>
2025-05-26 17:28:17 +0700

    finished ui dark and light

---
+++ src/Todo.js
@@ -0,0 +1,8 @@
+class Todo {
+  init() {
+    console.log("Todo app start");
+  }
+}
+
+
+export default Todo;
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import "./style.css";
+import "flowbite/dist/flowbite.min.js";
+import Todo from "./Todo";
+
+const todo = new Todo();
+
+todo.init();
